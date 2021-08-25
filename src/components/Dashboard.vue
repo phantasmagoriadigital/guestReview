857ae99064e1b48b9df1307a447bd6cc9aa591fa
esthercheran <mike@phantasmagoria.in>
2021-08-25 13:51:26 +0530

    State and app logic

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,23 +1,22 @@
 <template>
-    <div>
-        <h1>Know your guest</h1>
-        <v-icon>mdi-dots-horizontal-circle-outline</v-icon>
-        <v-form>
-            <v-text-field
-            v-model="phonenumber"
-            >
-
-            </v-text-field>
-        </v-form>
-    </div>
+  <div>
+    <h1>Know your guest</h1>
+    <v-icon>mdi-dots-horizontal-circle-outline</v-icon>
+    <v-form>
+      <v-text-field v-model="phonenumber"> </v-text-field>
+    </v-form>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Dashboard"
-    }
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      phonenumber: "value",
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
